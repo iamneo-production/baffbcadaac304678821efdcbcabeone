10a46ab0153ff7beab027f8f4a1e5755bbcb1867
Iamneo <techadmin@iamneo.ai>
2023-09-03 14:51:08 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-// JavaScript for Tic Tac Toe game
 
-let currentPlayer = "X"; // Initialize current player
-let board = ["", "", "", "", "", "", "", "", ""]; // Initialize the game board
-let gameOver = false; // Initialize game state
+let currentPlayer = "X"; 
+let board = ["", "", "", "", "", "", "", "", ""]; 
+let gameOver = false;
 
-// Function to make a move when a cell is clicked
-function makeMove(index) {
-    // Check if the game is not over and the clicked cell is empty
-    if (!gameOver && board[index] === "") {
-        // Update the board and display the player's symbol
+function makeMove(index)
+{
+   
+    if (!gameOver && board[index] === "")
+     {
+       
         board[index] = currentPlayer;
         document.getElementsByClassName("btn")[index].value = currentPlayer;
 
-        // Check if the current player has won
         checkWin();
 
-        // Toggle to the next player's turn
         currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
 }
 
-// Function to check if the current player has won
-function checkWin() {
+function checkWin() 
+{
     const winCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
     ];
 
-    // Iterate through winCombos to check for a win
-    for (const combo of winCombos) {
+    for (const combo of winCombos) 
+    {
         const [a, b, c] = combo;
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            // Update game state and display the winner
             gameOver = true;
             document.querySelector(".result-text").textContent = `${currentPlayer} wins!`;
             // Enable the reset button
