58703bcd4045381e80d0ba2046b07ef2b8a12647
Iamneo <techadmin@iamneo.ai>
2023-09-03 14:56:46 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -27,52 +27,45 @@ function checkWin()
     for (const combo of winCombos) 
     {
         const [a, b, c] = combo;
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        if (board[a] && board[a] === board[b] && board[a] === board[c])
+         {
             gameOver = true;
             document.querySelector(".result-text").textContent = `${currentPlayer} wins!`;
-            // Enable the reset button
             document.querySelector(".reset-button").disabled = false;
         }
     }
 
-    // Check for a draw (no empty cells left)
     if (!board.includes("") && !gameOver) {
-        // Update game state and display a draw message
         gameOver = true;
         document.querySelector(".result-text").textContent = "It's a draw!";
-        // Enable the reset button
         document.querySelector(".reset-button").disabled = false;
     }
 }
 
-// Function to reset the game
-function resetBoard() {
-    // Reset game variables and board
+function resetBoard() 
+{
     currentPlayer = "X";
     board = ["", "", "", "", "", "", "", "", ""];
     gameOver = false;
 
-    // Clear the board by setting button values to empty
     const buttons = document.getElementsByClassName("btn");
     for (const button of buttons) {
         button.value = "";
     }
 
-    // Reset the result text and disable the reset button
     document.querySelector(".result-text").textContent = "Player X's turn";
     document.querySelector(".reset-button").disabled = true;
 }
 
-// Add click event listeners to all buttons
 const buttons = document.getElementsByClassName("btn");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
+for (let i = 0; i < buttons.length; i++) 
+{
+    buttons[i].addEventListener("click", function() 
+    {
         makeMove(i);
     });
 }
 
-// Add click event listener to the reset button
 document.querySelector(".reset-button").addEventListener("click", resetBoard);
 
-// Initial game reset
 resetBoard();
